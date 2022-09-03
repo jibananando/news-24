8ec599b681554ba15f64608f5b1a77b2b875da59
jibananando <jibananando.pramanik@gmail.com>
2022-09-03 18:01:52 +0600

    added No result found cetagory

--- js/app.js
+++ js/app.js
@@ -15,34 +15,68 @@ const displayMenu = menubar => {
         `;
         menuContainer.appendChild(menuDiv);
     });
+
 }
+//spinner
+
+
 const loadCardDetail = async (code) => {
 
     const url = `https://openapi.programming-hero.com/api/news/category/0${code}`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayCardDetail(data.data);
+    const res = await fetch(url)
+    const data = await res.json()
+    displayCardDetail(data.data)
+    // Start spiner
+    toggleSpiner(true);
 }
 const displayCardDetail = (cards) => {
     const displayCard = document.getElementById('card-fields');
+    const textAlart = document.getElementById('no-data');
+    if (cards.length === 0) {
+        textAlart.classList.remove('d-none');
+    }
+    else {
+        textAlart.classList.add('d-none')
+    }
+    displayCard.textContent = '';
     cards.forEach(card => {
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
-        <div class="row g-0">
+        <div class="m-4 row g-0">
                     <div class="col-md-4">
                         <img src="${card.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${card.title}</h5>
-                            <p class="card-text">${card.details}</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="card-text text-truncate">${card.details}</p>
+                            <div class="card-footer d-flex justi justify-content-evenly ">
+                            <img src="${card.author.img}" style="width: 50px;" class="rounded-circle img-fluid m-2" alt="...">
+                            <h6>${card.author.name}<h6>
+                            <p><span>view: <span>${card.total_view}<p>
+                            </div>
                         </div>
                     </div>
                 </div>
         `;
         displayCard.appendChild(cardDiv);
-    })
+
+    });
+    // stop spiner
+    toggleSpiner(false);
+
 }
+
+// const toggleSpiner = isLoading => {
+//     const loaderSection = document.getElementById('loader');
+//     if (isLoading === true) {
+//         console.log('run');
+//         loaderSection.classList.remove('d-none');
+//     }
+//     else {
+//         console.log('not run');
+//         loaderSection.classList.add('d-none');
+//     }
+// }
 
 loadMenu();
